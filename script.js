5812d4cada076c66a826e64be103519323c992c8
Nadeem Nassar <nadeemkhaled9576@gmail.com>
2026-05-05 21:24:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -174,3 +174,20 @@ if (registerForm) {
         }
     });
 }
+/*form registertion*/
+document.getElementById("registerForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  localStorage.setItem("user", JSON.stringify({
+    username,
+    email,
+    password
+  }));
+
+  localStorage.setItem("isLoggedIn", "true");
+
+  window.location.href = "home.html";
